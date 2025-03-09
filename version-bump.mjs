d331ccdd2bbcb11b02b5f201da50eee3e11dbb38
Young Han Lee <joybro201@gmail.com>
2025-03-09 15:17:10 +0900

    Fix biome warnings

--- version-bump.mjs
+++ version-bump.mjs
@@ -1,14 +1,14 @@
-import { readFileSync, writeFileSync } from "fs";
+import { readFileSync, writeFileSync } from "node:fs";
 
 const targetVersion = process.env.npm_package_version;
 
-// 매니페스트 업데이트
-let manifest = JSON.parse(readFileSync("manifest.json", "utf8"));
+// update manifest
+const manifest = JSON.parse(readFileSync("manifest.json", "utf8"));
 manifest.version = targetVersion;
 writeFileSync("manifest.json", JSON.stringify(manifest, null, 2));
 
-// 버전 파일 업데이트
-let versions = JSON.parse(readFileSync("versions.json", "utf8"));
+// update versions file
+const versions = JSON.parse(readFileSync("versions.json", "utf8"));
 versions[targetVersion] = manifest.minAppVersion;
 writeFileSync("versions.json", JSON.stringify(versions, null, 2));
 
