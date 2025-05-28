a84a18ed01be9e8180feda679e424442b014ddcc
Young Han Lee <joybro201@gmail.com>
2025-05-28 22:29:56 +0900

    Delete debug log

--- src/domain/service/transformers.worker.ts
+++ src/domain/service/transformers.worker.ts
@@ -124,11 +124,6 @@ class TransformersWorker {
         const testEmbedding = tensor.tolist();
         this.vectorSize = testEmbedding[0].length;
 
-        console.log(
-            "== model_max_length",
-            this.extractor.tokenizer.model_max_length
-        );
-
         // Get max tokens from the tokenizer
         this.maxTokens = this.extractor.tokenizer.model_max_length ?? 512;
 
